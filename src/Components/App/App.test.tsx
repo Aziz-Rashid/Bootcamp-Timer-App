b77f9317eb50a83af213541640cd6124d2c8550c
aziz <axixraxhid@gmail.com>
2020-10-30 17:28:47 +0500

    first commit

---
+++ src/Components/App/App.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
+import Timer from '../Tmer/Timer'
+
+describe('App', () => {
+  it('should render a <div />', () => {
+    const container = shallow(<App />);
+    expect(container.find('div').length).toEqual(1);
+  });
+
+
+});
---
+++ src/Components/App/App.tsx
@@ -0,0 +1,19 @@
+import { Container } from '@material-ui/core';
+import React from 'react';
+import Timer from '../Tmer/Timer'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import "./App.css"
+function App() {
+  return (
+    <div>
+      <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="sm">
+          <Timer />
+        </Container>
+      </React.Fragment>
+    </div>
+  );
+}
+
+export default App;
